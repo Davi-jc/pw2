06e1ff8a25969f3bbcc1d487934a0a2949d806ba
Davi-JC <davi.chiquetti@unidavi.ed.br>
2025-03-06 20:49:35 -0300

    Ignorando vendor

---
+++ MeuProjetoPDF/index.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+use Mpdf\Mpdf;
+
+try {
+
+    $mpdf = new Mpdf();
+
+    $mpdf->WriteHTML('<h1>Teste do Exercico 3, este é um PDF gerado com mPDF!</h1>');
+
+    $mpdf->Output('meuArquivo.pdf', 'I'); 
+} catch (\Mpdf\MpdfException $e) {
+    echo 'Erro ao gerar PDF: ' . $e->getMessage();
+}
